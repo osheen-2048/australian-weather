34fc4edd49b2a5c8c77faef491f839278bd0998d
osheen-2048 <osheen+teachgit@jumpingrivers.com>
2025-06-06 12:03:16 +0100

    theme classic

--- R/create-plot.R
+++ R/create-plot.R
@@ -17,7 +17,7 @@ df %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point( colour = "blue") +
   ggtitle("Sydney") +
-  theme_minimal()
+  theme_classic()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
